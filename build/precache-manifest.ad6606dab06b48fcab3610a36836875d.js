9cefe9f154106a46a4ec53988da2542cf47bd0e4
Sahil Bhatia <sahil@joshsoftware.com>
2019-09-02 16:25:46 +0530

    added build

---
+++ build/precache-manifest.ad6606dab06b48fcab3610a36836875d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca01ba054d28bca143495d4bc45e8a9",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "7bfa2cb75bc2d3ffbe48",
+    "url": "/build/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "20f05e212bf9e41475fb",
+    "url": "/build/static/css/main.36ff4d79.chunk.css"
+  },
+  {
+    "revision": "7bfa2cb75bc2d3ffbe48",
+    "url": "/build/static/js/2.541e9762.chunk.js"
+  },
+  {
+    "revision": "20f05e212bf9e41475fb",
+    "url": "/build/static/js/main.12f78ba0.chunk.js"
+  },
+  {
+    "revision": "0c48433bbb19c2cd727e",
+    "url": "/build/static/js/runtime~main.e079702a.js"
+  }
+]);
